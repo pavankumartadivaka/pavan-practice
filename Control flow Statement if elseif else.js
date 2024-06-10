f0151f47c907a071bc44fc68a67e8a270024632a
pavankumartadivaka <125035951+pavankumartadivaka@users.noreply.github.com>
2024-06-10 17:43:14 +0530

    Add files via upload

---
+++ Control flow Statement if elseif else.js
@@ -0,0 +1,10 @@
+let num=20;
+if(num>0){
+    console.log("Given number is greeter than Zero")
+}
+else if(num<0){
+    console.log("Given number is lesser than Zero")
+}
+else{
+    console.log("Given number is Zero")
+}
