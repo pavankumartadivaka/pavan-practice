b588e67919ab2d13349e6b4deb6de0b833c7aadc
pavankumartadivaka <125035951+pavankumartadivaka@users.noreply.github.com>
2024-06-10 10:25:34 +0530

    Add files via upload

---
+++ Arithmetic operators.js
@@ -0,0 +1,11 @@
+let a=20;
+let b=10;
+console.log(a+b)
+console.log(a-b)
+console.log(a*b)
+console.log(a/b)
+console.log(a%b)
+console.log(a++)
+console.log(--a)
+console.log(b++)
+console.log(--b)
