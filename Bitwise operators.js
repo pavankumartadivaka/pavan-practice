41f48a5b8fb0705669ece068805cfc486ce0deb2
pavankumartadivaka <125035951+pavankumartadivaka@users.noreply.github.com>
2024-06-10 13:56:08 +0530

    Add files via upload

---
+++ Bitwise operators.js
@@ -0,0 +1,12 @@
+let a=20;
+let b=10;
+let c=30;
+let d=40;
+console.log(a>b&c<d)
+console.log(a<b&c>d)
+console.log(a>b|c<d)
+console.log(a<b|c<d)
+console.log(a>b^c<d)
+console.log(a<b^c>d)
+console.log(~a)
+console.log(~b)
