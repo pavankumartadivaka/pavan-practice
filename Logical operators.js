a5b5a5805653e4a367eaf534b786dbbf3f156746
pavankumartadivaka <125035951+pavankumartadivaka@users.noreply.github.com>
2024-06-10 16:20:43 +0530

    Add files via upload

---
+++ Logical operators.js
@@ -0,0 +1,16 @@
+let a=20;
+let b=10;
+let c=40;
+let d=30;
+console.log(a>b&&c>d)
+console.log(a<b&&c<d)
+console.log(a>b&&c<d)
+console.log(a<b&&c>d)
+console.log(a>b||c>d)
+console.log(a<b||c<d)
+console.log(a>b||c<d)
+console.log(a<b||c>d)
+console.log(!(a==b))
+console.log(!(c==d))
+console.log(!(a>b))
+console.log(!(a<b))
