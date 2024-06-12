773e59edb3b5b156ce68bb68ef7307ff20e59c6b
pavankumartadivaka <125035951+pavankumartadivaka@users.noreply.github.com>
2024-06-12 13:02:09 +0530

    Add files via upload

---
+++ constructor Array.js
@@ -0,0 +1,10 @@
+let Courses=new Array("HTML","CSS","JavaScript","Reactjs");
+console.log(Courses);
+
+let arr=new Array(5);
+arr[0]=10;
+arr[1]=20;
+arr[2]=30;
+arr[3]=40;
+arr[4]=50;
+console.log(arr);
